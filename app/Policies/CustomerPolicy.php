be8030fff47b5b5faee7851c8871649a36c300aa
Jakub Gazdowicz <jakub-gazdowicz@wp.pl>
2023-03-04 09:57:34 +0100

    Created Customer Policy

---
+++ app/Policies/CustomerPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CustomerPolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Customer $customer): bool
+    {
+        return $user->id === $customer->user_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Customer $customer): bool
+    {
+        return $user->id === $customer->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Customer $customer): bool
+    {
+        return $user->id === $customer->user_id;
+    }
+}
