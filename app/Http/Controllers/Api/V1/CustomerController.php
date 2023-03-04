4757ca41bb97201a6da2b95777a6e78077d7541b
Jakub Gazdowicz <jakub-gazdowicz@wp.pl>
2023-03-04 10:08:33 +0100

    Created Customer Controller

---
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Http\Resources\V1\CustomerResource;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        return new CustomerCollection(Customer::paginate(10));
+    }
+
+    public function show(Customer $customer, Request $request)
+    {
+
+
+        return new CustomerResource($customer);
+    }
+
+    public function store(StoreCustomerRequest $request)
+    {
+        Customer::create($request->validated());
+
+        return response()->json('Customer created');
+    }
+
+    public function update(StoreCustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->validated());
+
+        return response()->json('customer updated');
+    }
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+
+        return response()->json("Customer deleted");
+    }
+}
